906781ed8c1056c993a7dece2180b43b650ad716
vroxo <vitorroxo.dev@gmail.com>
2022-04-14 20:12:02 +0000

    refactor: :recycle: Use class validator in Category entity

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,6 +1,7 @@
 import UniqueEntityId from '../../../@seedwork/domain/value-objects/unique-entity-id.vo'
 import Entity from '../../../@seedwork/domain/entity/entity'
 import ValidatorRules from '../../../@seedwork/validators/validator-rules';
+import CategoryValidatorFactory from '../../validators/category.validator';
 
 export type CategoryProperties = {
     name: string;
@@ -29,11 +30,16 @@ export class Category extends Entity<CategoryProperties> {
         this.props.description = description;
     }
 
-    static validate(props: Omit<CategoryProperties, 'id' | 'created_at'>) {
-        ValidatorRules.values(props.name, 'name').required().string().maxLength(255);
-        ValidatorRules.values(props.description, 'description').string();
-        ValidatorRules.values(props.is_active, 'is_active').boolean();
+    static validate(props: CategoryProperties){
+        const validator = CategoryValidatorFactory.create();
+        validator.validate(props);
     }
+
+    // static validate(props: Omit<CategoryProperties, 'id' | 'created_at'>) {
+    //     ValidatorRules.values(props.name, 'name').required().string().maxLength(255);
+    //     ValidatorRules.values(props.description, 'description').string();
+    //     ValidatorRules.values(props.is_active, 'is_active').boolean();
+    // }
 
     deactivate() {
         this.props.is_active = false;
