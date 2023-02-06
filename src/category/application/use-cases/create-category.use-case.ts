1c5f69dbcc410d21b82bd48942f309f02fe8110c
Vitor Roxo <vitorroxo@pop-os.localdomain>
2023-02-06 16:35:18 -0300

    feat: create a create category use case

---
+++ src/category/application/use-cases/create-category.use-case.ts
@@ -0,0 +1,35 @@
+import CategoryRepository from "../../domain/repository/category.repository";
+import { Category } from "../../domain/entities/category";
+
+export default class CreateCategoryUseCase {
+  constructor(private categoryRepo: CategoryRepository.Repository){
+
+  }
+
+  async execute(input: Input): Promise<Output> {
+    const entity = new Category(input);
+    await this.categoryRepo.insert(entity);
+    return {
+      id: entity.id,
+      name: entity.name,
+      description: entity.description,
+      is_active: entity.is_active,
+      created_at: entity.created_at
+    }
+  }
+
+}
+
+export type Input = {
+  name: string;
+  description?: string;
+  is_active?: boolean;
+}
+
+export type Output = {
+  id: string;
+  name: string;
+  description?: string;
+  is_active?: boolean;
+  created_at: Date;
+}
